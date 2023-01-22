e7719273f70bfaa00b14fe2f88eb4ce82e90212a
Sofia Katzman <katzmansof@gmail.com>
2023-01-21 21:23:26 -0500

    sof completed sat

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+   cats.unshift(name);
+
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    return [...cats, name];
+    // return it as new array
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+    // return it as new array
+}
+
+function removeLastCat() {
+   return [...cats.slice(0,-1)];
+    
+}
+
+function removeFirstCat() {
+    return [...cats.slice(1)];
+     
+ }
